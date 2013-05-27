fe07f36a1a21b62c72b36ebd91e0e7a00965ac89
Liangent <liangent@tools.wmflabs.org>
2013-05-27 05:22:57 +0000

    Add initial cleanupILH.php

---
+++ cleanupILH.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ */
+
+require_once( dirname( __FILE__ ) . '/Maintenance.php' );
+
+class CleanupILH extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'maxlag', 'Do not run if DB lags more than this time.' );
+	}
+
+	static function fallbackArray( &$a, $b ) {
+		for ( $i = 0; $i < count( $a ); $i++ ) {
+			if ( !$a[$i] ) {
+				$a[$i] = $b[$i];
+			}
+		}
+	}
+
+	public function cleanupTitle( $title, $ident = '', $recur = true ) {
+		global $wgContLang, $wgLabs;
+
+		static $cleanedup = array();
+		static $parserOutput = null;
+
+		$this->output( $ident . $title->getPrefixedText() );
+		if ( !$title->exists() ) {
+			$this->output( "\tredlink.\n" );
+			return;
+		}
+
+		if ( $title->isProtected( 'edit' ) && !( $wgLabs->user->isAllowed( 'protect' ) ) ) {
+			# It's unlikely to be problematic, and usually we don't have the right
+			# to clean them up ( = edit them )...
+			$this->output( "\tprotected.\n" );
+			return;
+		}
+
+		if ( isset( $cleanedup[$title->getPrefixedDBKey()] ) ) {
+			$this->output( "\tskipping...\n" );
+			return;
+		} else {
+			$cleanedup[$title->getPrefixedDBKey()] = true;
+		}
+
+		$otext = $text = Revision::newFromTitle( $title )->getText();
+		# First, find out all links to check
+		$matches = array();
+		$lang = '(?:ar|de|en|es|fi|fr|it|ja|ms|nl|no|pl|pt|ru|sv|ko|vi|tr|da)';
+		$ilhRe = '/\{\{\s*(?:Internal[_ ]link[_ ]helper\/' . $lang
+			. '|Link-' . $lang . '|' . $lang . '-link)\s*'
+			. '(?:\|\s*([^|}]*?)\s*)?' # Local page name
+			. '(?:\|\s*([^|}]*?)\s*)?' # Interwiki page name
+			. '(?:\|\s*([^|}]*?)\s*)?' # Text
+			. '\}\}/i';
+		$transRe = '/\{\{\s*(?:Translink|Tsl)\s*'
+			. '(?:\|\s*[^|}]*\s*)?' # Lang
+			. '(?:\|\s*([^|}]*?)\s*)?' # Interwiki page name
+			. '(?:\|\s*([^|}]*?)\s*)?' # Local page name
+			. '(?:\|\s*([^|}]*?)\s*)?' # Text
+			. '\}\}/i';
+		$batch = new LinkBatch();
+		$titles = array();
+		preg_match_all( $ilhRe, $text, $matches, PREG_PATTERN_ORDER );
+		list( $templates, $locals, $interwikis, $descs ) = $matches;
+		self::fallbackArray( $interwikis, $locals );
+		preg_match_all( $transRe, $text, $matches, PREG_PATTERN_ORDER );
+		list( $templates2, $interwikis2, $locals2, $descs2 ) = $matches;
+		self::fallbackArray( $locals2, $interwikis2 );
+		$templates = array_merge( $templates, $templates2 );
+		$interwikis = array_merge( $interwikis, $interwikis2 );
+		$locals = array_merge( $locals, $locals2 );
+		$descs = array_merge( $descs, $descs2 );
+		$lb = new LinkBatch( $titles );
+		foreach ( $locals as $titleText ) {
+			$lb->addObj( $titles[] = Title::newFromText( $titleText ) );
+			foreach ( $wgContLang->autoConvertToAllVariants( $titleText ) as $titleText ) {
+				$lb->addObj( Title::newFromText( $titleText ) );
+			}
+		}
+		$lb->execute();
+		for ( $i = 0; $i < count( $templates ); $i++ ) {
+			$wgContLang->findVariantLink( $locals[$i], $titles[$i] );
+			if ( $titles[$i] && $titles[$i]->isKnown() ) {
+				$replace = '[[';
+				if ( $titles[$i]->getNamespace() == NS_FILE
+					|| $titles[$i]->getNamespace() == NS_CATEGORY
+				) {
+					$replace .= ':';
+				}
+				if ( $descs[$i] ) {
+					$nt = Title::newFromText( $descs[$i] );
+					if ( $nt && $nt->equals( $titles[$i] ) ) {
+						$replace .= $descs[$i];
+					} else {
+						$replace .= $titles[$i]->getPrefixedText() . '|' . $descs[$i];
+					}
+				} else {
+					$replace .= $titles[$i]->getPrefixedText();
+				}
+				$replace .= ']]';
+				$text = str_replace( $templates[$i], $replace, $text );
+			}
+		}
+
+		if ( $text === $otext ) {
+			$this->output( "\tno change." );
+			if ( $recur ) {
+				$this->output( '..' );
+				if ( !$parserOutput ) {
+					$parserOutput = new ParserOutput();
+				}
+				$u = new LinksUpdate( $title, $parserOutput );
+				$u->doUpdate();
+				$this->output( ' linksupdated.' );
+			}
+			$this->output( "\n" );
+		} else {
+			$this->output( "\tsaving..." );
+			if ( WikiPage::factory( $title )->doEdit( $text,
+				wfMessage( 'ts-cleanup-ilh' )->text(),
+			EDIT_MINOR )->isOK() ) {
+				$this->output( " done.\n" );
+			} else {
+				$this->output( " ERROR.\n" );
+			}
+		}
+		if ( $text === $otext && $recur ) {
+			# We should call cleanupTitle on all templates this page uses.
+			# Blue links may be there, maybe invisible from parsed view!
+			# But don't do this recursively?
+			foreach ( $title->getTemplateLinksFrom() as $template ) {
+				$this->cleanupTitle( $template, $ident . '  ', false );
+			}
+		}
+	}
+
+	public function execute() {
+		global $wgContLang;
+		if ( $this->hasOption( 'maxlag' ) ) {
+			$maxlag = intval( $this->getOption( 'maxlag' ) );
+			$lag = wfReplag();
+			if ( $lag > $maxlag ) {
+				$this->output( "Current lag: $lag, required maxlag: $maxlag, exiting.\n" );
+				return;
+			}
+		}
+		$it = new AppendIterator();
+		$it->append( Category::newFromName( '有蓝链却未移除内部链接助手模板的页面' )->getMembers() );
+		$it->append( Category::newFromName( 'Trans-Clean' )->getMembers() );
+		foreach ( $it as $title ) {
+			$this->cleanupTitle( $title );
+		}
+		$this->output( "done\n" );
+	}
+}
+
+$maintClass = "CleanupILH";
+require_once( RUN_MAINTENANCE_IF_MAIN );
