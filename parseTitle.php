276d8c7e66b7665f3c8a45fb7094d635a9d38960
Liangent <liangent@tools.wmflabs.org>
2013-09-23 08:51:00 +0000

    New parseTitle.php script for internal use

---
+++ parseTitle.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once( dirname( __FILE__ ) . '/Maintenance.php' );
+
+class ParseTitle extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function executeLine( $line ) {
+		$title = Title::newFromText( $line );
+		$data = array();
+
+		if ( $title ) {
+			$data['interwiki'] = $title->getInterwiki();
+			$data['namespace'] = $title->getNamespace();
+			$data['dbkey'] = $title->getDBKey();
+		}
+
+		$this->output( FormatJson::encode( $data ) );
+		$this->output( "\n" );
+	}
+
+	public function execute() {
+		foreach ( $this->mArgs as $line ) {
+			$this->executeLine( $line );
+		}
+	}
+}
+
+$maintClass = "ParseTitle";
+require_once( RUN_MAINTENANCE_IF_MAIN );
