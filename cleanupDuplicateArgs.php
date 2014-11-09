03d99c85160d16105a4c465eead2b8c11390c069
Liangent <liangent@tools.wmflabs.org>
2014-11-09 08:31:59 +0000

    Add cleanupDuplicateArgs.php

---
+++ cleanupDuplicateArgs.php
@@ -0,0 +1,102 @@
+<?php
+
+require_once( dirname( __FILE__ ) . '/PageDomMaintenance.php' );
+
+class CleanupDuplicateArgs extends PageDomMaintenance {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function executeTitleDom( $title, $dom, $rev, $data ) {
+		$this->domModified = false;
+		$this->title = $title;
+		$text = $this->nodeToWikitext( $dom );
+		if ( $this->domModified ) {
+			$this->output( "saving..." );
+			if ( WikiPage::factory( $title )->doEdit( $text,
+				wfMessage( 'ts-cleanup-dupargs' )->text(), EDIT_MINOR,
+				$rev ? $rev->getId() : false
+			)->isOK() ) {
+				$this->output( " done.\n" );
+			} else {
+				$this->output( " ERROR.\n" );
+			}
+		} else {
+			$this->output( "no change.\n" );
+		}
+	}
+
+	public function executeTemplate( $node, $arrayNode ) {
+		$args = array();
+		$pieces = array( '{{' );
+		for ( $i = 0; $i < $arrayNode->getLength(); $i++ ) {
+			$childNode = $arrayNode->item( $i );
+			switch ( $childNode->getName() ) {
+			case 'title':
+				$pieces[] = $this->nodeToWikitext( $childNode );
+				break;
+			case 'part':
+				$arg = $childNode->splitArg();
+				if ( $arg['index'] ) {
+					$argkr = $argk = $arg['index'];
+					$argvr = $argv = $this->nodeToWikitext( $arg['value'] );
+				} else {
+					$argk = $this->nodeToWikitext( $arg['name'] );
+					$argkr = trim( $argk );
+					$argv = $this->nodeToWikitext( $arg['value'] );
+					$argvr = trim( $argv );
+				}
+				if ( isset( $args[$argkr][$argvr] ) ) {
+					# Drop this argument.
+					$this->domModified = true;
+					$this->output( "Dropping duplicated argument: $argk=$argv\n" );
+					break;
+				}
+				$args[$argkr][$argvr] = 0;
+				$pieces[] = '|';
+				if ( !$arg['index'] ) {
+					$pieces[] = $argk;
+					$pieces[] = '=';
+				}
+				$pieces[] = $argv;
+				break;
+			}
+		}
+		$pieces[] = '}}';
+		return implode( '', $pieces );
+	}
+
+	public function executeExt( $node, $arrayNode ) {
+		$ext = $node->splitExt();
+		$pieces = array( '<' );
+		$pieces[] = $name = $this->nodeToWikitext( $ext['name'] );
+		$pieces[] = $this->nodeToWikitext( $ext['attr'] );
+		if ( isset( $ext['inner'] ) ) {
+			$pieces[] = '>';
+			$inner = $this->nodeToWikitext( $ext['inner'] );
+			if ( in_array( $name, array( 'ref', 'references' ) ) ) {
+				global $wgParser;
+				$parser = $wgParser->getFreshParser();
+				$parser->startExternalParse( $this->title, new ParserOptions, OT_PREPROCESS );
+				$dom = $wgParser->preprocessToDom( $inner );
+				if ( !( $dom instanceof PPNode_DOM ) ) {
+					$dom = RemoteUtils::preprocessXmlToDom( $dom->__toString() );
+				}
+				$inner = $this->nodeToWikitext( $dom );
+			}
+			$pieces[] = $inner;
+		}
+		if ( isset( $ext['close'] ) ) {
+			$pieces[] = $this->nodeToWikitext( $ext['close'] );
+		} elseif ( isset( $ext['inner'] ) ) {
+			throw new MWException( 'Unexpected <ext> structure' );
+		} else {
+			$pieces[] = '/>';
+		}
+		return implode( '', $pieces );
+	}
+}
+
+$maintClass = "CleanupDuplicateArgs";
+require_once( RUN_MAINTENANCE_IF_MAIN );
