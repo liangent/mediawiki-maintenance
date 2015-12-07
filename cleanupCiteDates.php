de8a9de3a973feebc259fd92af1eec892698e654
Liangent <liangent@tools.wmflabs.org>
2015-12-07 22:16:08 +0000

    cleanupCiteDates: Fix full width commas

--- cleanupCiteDates.php
+++ cleanupCiteDates.php
@@ -103,6 +103,14 @@ class CleanupCiteDates extends PageDomMaintenanceExt {
 			return $date;
 		}
 
+		# Punctuations?
+
+		$date = preg_replace( '/(，|,) */', ', ', $date );
+
+		if ( $date !== $original && $this->validateDateString( $date ) ) {
+			return $date;
+		}
+
 		# Aggressive rules -- the resulting date will be revalidated anyway.
 
 		$date = preg_replace( '/, *| +/', ' ', $date );
