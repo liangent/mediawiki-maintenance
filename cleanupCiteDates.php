ca1d731778cbb61cf6cb20b0a57837fff0a6eac8
Liangent <liangent@tools.wmflabs.org>
2015-12-14 15:13:18 +0000

    cleanupCiteDates: replace "impossible" punctuations earlier

--- cleanupCiteDates.php
+++ cleanupCiteDates.php
@@ -80,6 +80,10 @@ class CleanupCiteDates extends PageDomMaintenanceExt {
 		$date = trim( html_entity_decode( preg_replace( '/&nbsp;/', ' ', $date ) ) );
 		$date = preg_replace( '/\[\[([^<>\[\]\|\{\}]+)\]\]/', '\1', $date );
 		$date = mb_convert_kana( $date, 'rnas', 'utf-8' ); // full-width to half-width
+		$date = strtr( $date, array(
+			'、' => ',',
+			'。' => '.',
+		) );
 		$date = preg_replace( '/^(于|於)\s*|\s*(查阅|查閱|出版)$/', '', $date );
 		$date = preg_replace( '/(\d)(st|nd|rd|th)\b/', '\1', $date );
 
@@ -143,7 +147,7 @@ class CleanupCiteDates extends PageDomMaintenanceExt {
 
 		# Punctuations?
 
-		$date = preg_replace( '/(､|,) */', ', ', $date );
+		$date = preg_replace( '/, */', ', ', $date );
 
 		if ( $date !== $original && $this->validateDateString( $original = $date ) ) {
 			return $date;
